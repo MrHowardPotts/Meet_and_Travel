6959e827dc2ed399e81d3bac960e7df8dbe9f336
lukastanisic99 <lukastanisic2@gmail.com>
2021-05-08 23:38:23 +0200

    Chat backend with Thread listener implemented

---
+++ src/Message.php
@@ -0,0 +1,29 @@
+<?php
+//klasa za povezivanje sa bazom podataka
+class Message implements JsonSerializable {
+    
+    private $idmessage;
+    private $idgroup;
+    private $iduser;
+    private $message;
+    
+
+    public function __construct($idm,$idg,$idu,$message) {
+        $this->idmessage=$idm;
+        $this->idgroup=$idg;
+        $this->iduser=$idu;
+        $this->message=$message;
+    }
+
+    private function __clone() {}
+
+    public function jsonSerialize()
+    {
+        return [
+            'idmessage'=>$this->idmessage,'idgroup'=>$this->idgroup,
+            'iduser'=>$this->iduser,'message'=>$this->message
+        ];
+    }
+    
+  }
+?>
